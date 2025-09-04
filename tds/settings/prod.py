3d3b9a655bda31612a5a0ac70b8e134e58fb9dd4
Marc <mtakoumba@gmail.com>
2025-09-04 18:01:22 +0200

    correction de setting

--- tds/settings/prod.py
+++ tds/settings/prod.py
@@ -28,6 +28,9 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+USE_X_FORWARDED_HOST = True
+
 # ─── Stockage Scaleway S3 ───────────────────────────────────────
 STORAGE_BACKEND = "aws"
 
